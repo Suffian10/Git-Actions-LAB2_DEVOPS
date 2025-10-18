87039a7e3d0cbd465e626a3c2051f055823bafc0
Muhammad Suffian <99536763+Suffian10@users.noreply.github.com>
2025-10-18 14:28:04 +0500

    Update greeting message in showMessage function

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 function showMessage() {
-  document.getElementById('message').innerText = "Hello, Jahanzaib! This is your GitHub Pages site 😎";
+  document.getElementById('message').innerText = "Hello, SUFFIAN This is your GitHub Pages site 😎";
 }
